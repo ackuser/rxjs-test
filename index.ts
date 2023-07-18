d089485ac400422beebc2b15522dab464d278fb2
ackuser <mail@ackarim.com>
2023-07-18 17:28:17 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { combineLatest, from, interval, merge, of } from 'rxjs';
+import { combineLatest, forkJoin, from, interval, merge, of, zip } from 'rxjs';
 import {
   concatMap,
   first,
@@ -9,47 +9,67 @@ import {
   sample,
   startWith,
   withLatestFrom,
-  combineLatestWith
+  combineLatestWith,
 } from 'rxjs/operators';
-//emit value every 1s
-const source1$ = of(['x', 'y']).pipe(startWith(''));
-//sample last emitted value from source every 2s
-const source2$ = from(['a', 'b', 'c']).pipe(startWith(''));
-//output: 2..4..6..8..
-const source3$ = combineLatest(source1$, source2$).pipe(
-  map((x, y) => {
-    return '' + x;
-  })
-);
 
-let source1From$ = from(['a', 'b', 'c'])
-let source1Of$ = of(['a', 'b', 'c'])
-let source2From$ = from(['x','y'])
-let source2Of$ = of(['x','y'])
+let source1From$ = from(['a', 'b', 'c']);
+let source2From$ = from(['x', 'y']);
+let source1Of$ = of(['a', 'b', 'c']);
+let source2Of$ = of(['x', 'y']);
 
-console.log('---------------')
+console.log('---------------');
 source1From$.subscribe((val) => console.log(val));
-console.log('---------------')
+console.log('---------------');
 source1Of$.subscribe((val) => console.log(val));
-console.log('---------------')
+console.log('---------------');
 source2From$.subscribe((val) => console.log(val));
-console.log('---------------')
+console.log('---------------');
 source2Of$.subscribe((val) => console.log(val));
-console.log('---------------')
+console.log('---------------');
 combineLatest(source1From$, source2From$).subscribe((val) => console.log(val));
-console.log('---------------')
+console.log('---------------');
 combineLatest(source1From$, source2From$).subscribe((val) => console.log(val));
-console.log('---------------')
-source1From$.pipe(combineLatestWith(source2From$)).subscribe((val) => console.log(val));
+console.log('---------------');
+source1From$
+  .pipe(combineLatestWith(source2From$))
+  .subscribe((val) => console.log(val));
 
+console.log('---------------');
+source1Of$
+  .pipe(combineLatestWith(source2Of$))
+  .subscribe((val) => console.log(val));
 
-combineLatest(from(['a', 'b', 'c']),)
+console.log('---------------');
+source1From$
+  .pipe(
+    concatMap(
+      (_) => {
+        console.log(_);
+        return from(source2From$);
+      },
+      (a, b) => ({
+        a,
+        b,
+      })
+    )
+  )
+  .subscribe((val) => console.log(val));
 
-//source3$.subscribe((val) => console.log(val));
+console.log('---------------');
+zip(source1From$, source2From$).subscribe((val) => console.log(val));
+console.log('---------------');
+zip(source1Of$, source2Of$).subscribe((val) => console.log(val));
+console.log('---------------');
+zip(source1Of$, source2From$).subscribe((val) => console.log(val));
 
-const source4$ = withLatestFrom([
-  //of(0,1,2),
-  source1$.pipe(startWith(null)),
-  source2$.pipe(startWith(null)),
-]);
+console.log('---------------');
+source1From$
+  .pipe(withLatestFrom(source2From$))
+  .subscribe((val) => console.log(val));
 
+console.log('forkJoin---------------From');
+forkJoin(source1From$, source2From$.pipe(startWith(0))).subscribe((val) =>
+  console.log(val)
+);
+console.log('forkJoin---------------Of');
+forkJoin(source1Of$, source2Of$).subscribe((val) => console.log(val));
